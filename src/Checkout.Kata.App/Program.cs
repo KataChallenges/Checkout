beb57eca10e8c4fa1378b02e1c47988ab731298f
Zeeshan Tariq <zeeshan.tariq@vanquisbank.co.uk>
2021-03-06 21:48:21 +0000

    Initial Solution Commit

---
+++ src/Checkout.Kata.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkout.Kata.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Checkout Kata Challenge!");
+        }
+    }
+}
